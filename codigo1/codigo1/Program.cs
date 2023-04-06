bd633a7d20d9e6ce7c92693060af97e38952cac6
LucasCNS <celionuvem@gmail.com>
2023-04-06 20:03:16 -0300

    DEV - Add primeiro código do processo seletivo.

---
+++ codigo1/codigo1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace codigo1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int indice = 13;
+			int soma = 0;
+			int k = 0;
+
+			for (int i = 0; i < indice; i++)
+			{
+				k = k + 1;
+				soma = soma + k;
+			}
+
+			Console.WriteLine($"{soma}");
+			Console.ReadKey();
+
+		}
+	}
+}
