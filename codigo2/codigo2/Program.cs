c7507f7247f626a048aa21f631a23d6a0bbef3b7
LucasCNS <celionuvem@gmail.com>
2023-04-06 21:27:44 -0300

    DEV - exercício 2 - Procurar N na sequência Fibonacci.

---
+++ codigo2/codigo2/Program.cs
@@ -0,0 +1,48 @@
+﻿
+namespace codigo2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			CalculaFibonacci();
+		}
+
+		static void CalculaFibonacci()
+		{
+			Console.Write("Digite um número para a sequência de Fibonacci: ");
+			int n = int.Parse(Console.ReadLine());
+
+			int a = 0, b = 1, c;
+
+			Console.Write(a + " " + b + " ");
+
+
+			int i = 2;
+			while (true)
+			{
+				c = a + b;
+				Console.Write(c + " ");
+
+				if (c == n)
+				{
+					Console.WriteLine($"\nO número {n} está na sequência de Fibonacci.");
+					break;
+				}
+				else if (c > n)
+				{
+					Console.WriteLine($"\nO número {n} não está na sequência de Fibonacci.");
+					break;
+				}
+
+				c = a + b;
+				a = b;
+				b = c;
+				i++;
+			}
+
+		}
+
+		
+	}
+}
