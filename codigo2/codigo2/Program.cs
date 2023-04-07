e2c4d758eee67ae5ad8718e185d77b844037e1ef
LucasCNS <celionuvem@gmail.com>
2023-04-06 21:40:31 -0300

    DEV - Add desafio 4, informar porcentagem.

--- codigo2/codigo2/Program.cs
+++ codigo2/codigo2/Program.cs
@@ -5,7 +5,10 @@ namespace codigo2
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Desafio 2\n");
 			CalculaFibonacci();
+			Console.WriteLine("\nDesafio 4");
+			PercentualEstados();
 		}
 
 		static void CalculaFibonacci()
@@ -43,6 +46,26 @@ namespace codigo2
 
 		}
 
-		
+		static void PercentualEstados()
+		{
+			{
+				double sp = 67836.43;
+				double rj = 36678.66;
+				double mg = 29229.88;
+				double es = 27165.48;
+				double outros = 19849.53;
+				double total = sp + rj + mg + es + outros;
+
+				Console.WriteLine("\nValor de representação de cada estado no faturamento mensal:");
+				Console.WriteLine($"SP: {sp}, RJ: {rj}, MG: {mg}, ES: {es} ");
+
+				Console.WriteLine("\nPercentual de representação de cada estado no faturamento mensal:");
+				Console.WriteLine("SP: " + (sp / total * 100).ToString("F2") + "%");
+				Console.WriteLine("RJ: " + (rj / total * 100).ToString("F2") + "%");
+				Console.WriteLine("MG: " + (mg / total * 100).ToString("F2") + "%");
+				Console.WriteLine("ES: " + (es / total * 100).ToString("F2") + "%");
+				Console.WriteLine("Outros: " + (outros / total * 100).ToString("F2") + "%\n");
+			}
+		}
 	}
 }
